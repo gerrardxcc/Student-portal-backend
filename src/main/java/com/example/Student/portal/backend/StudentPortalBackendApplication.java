069d793c3a6e1772d90baaa88255914863bed7e7
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-02-22 18:02:03 +1000

    Import RestController and GetMapping to connect with RestfulAPI.

--- src/main/java/com/example/Student/portal/backend/StudentPortalBackendApplication.java
+++ src/main/java/com/example/Student/portal/backend/StudentPortalBackendApplication.java
@@ -2,12 +2,19 @@ package com.example.Student.portal.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentPortalBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentPortalBackendApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello(){
+		return "Hello World";
+	}
 }
